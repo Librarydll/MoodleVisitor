f030b960ca7bb76052c4d45136cd4d9308bc944c
Komil <library.dll@mail.ru>
2020-04-10 19:22:03 +0500

    Add project files.

---
+++ MoodleVisitor/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MoodleVisitor.Views;
+using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+using Unity;
+namespace MoodleVisitor
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : PrismApplication
+	{
+		protected override Window CreateShell()
+		{
+			return (Window)Container.Resolve(typeof(MainView));
+		}
+		protected override void RegisterTypes(IContainerRegistry containerRegistry)
+		{
+
+		}
+	}
+}
